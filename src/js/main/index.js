d8277e99cd02c0c80da90ffee0e5e476fb1e4b5e
boysangurkha <r0797383@student.thomasmore.be>
2022-12-07 21:50:54 +0100

    Feat: 'Donut Maker' flies in and changes color contrary to bg

--- src/js/main/index.js
+++ src/js/main/index.js
@@ -1,10 +1,14 @@
 let bg = document.querySelector('.bg');
+let h1 = document.querySelector('.h1Container');
 
 let color = ['#82D1E4', '#F7F249', '#E72C70', '#F7F249'];
+let bgColor = ['#F7F249', '#82D1E4', '#F7F249', '#E72C70'];
 
 let i = 0;
 setInterval(function(){
-    bg.style.backgroundColor = color[i];
+    bg.style.backgroundColor = color[i]; 
+    h1.style.color = bgColor[i];
+
     i++;
     if(i == color.length){
         i = 0;
