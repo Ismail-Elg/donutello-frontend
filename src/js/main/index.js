64b3ee8f280855a8c7aa0c3bc5a0234d2d8410dd
boysangurkha <r0797383@student.thomasmore.be>
2022-12-07 21:53:25 +0100

    Feat: Added button 'Smullen'

--- src/js/main/index.js
+++ src/js/main/index.js
@@ -1,4 +1,5 @@
 let bg = document.querySelector('.bg');
+let buttonBg = document.querySelector('.button');
 let h1 = document.querySelector('.h1Container');
 
 let color = ['#82D1E4', '#F7F249', '#E72C70', '#F7F249'];
@@ -6,7 +7,9 @@ let bgColor = ['#F7F249', '#82D1E4', '#F7F249', '#E72C70'];
 
 let i = 0;
 setInterval(function(){
-    bg.style.backgroundColor = color[i]; 
+    bg.style.backgroundColor = color[i];
+    buttonBg.style.backgroundColor = bgColor[i];
+    buttonBg.style.color = color[i]; 
     h1.style.color = bgColor[i];
 
     i++;
