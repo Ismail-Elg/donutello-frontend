1c24c61d3973fdc195eff74ddcfa65d2b0290de7
boysangurkha <r0797383@student.thomasmore.be>
2022-12-11 01:57:01 +0100

    Feat: Added loading screen for configurator

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
 import "./src/assets/sass/style.scss"
+import "/src/js/config/loading.js";
 
 import Scene from "/src/js/config";
 
 const scene = new Scene();
 scene.init();
+
+  
+
+
---
+++ src/js/config/loading.js
@@ -0,0 +1,20 @@
+let config = document.querySelector(".configurator");
+config.style.display = "none";
+let loading = document.querySelector(".loadingContainer");
+
+var loading2 = bodymovin.loadAnimation({
+    container: document.getElementById('loading'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'load.json'
+});
+
+window.onload = function(){
+    setTimeout(loadAfterTime, 3500);
+};
+
+function loadAfterTime(){
+    loading.style.display = "none";
+    config.style.display = "block";
+}
