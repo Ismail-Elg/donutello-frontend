63cd20cb62bca90962c48e19e2df1a9c0790d199
boysangurkha <r0797383@student.thomasmore.be>
2022-12-12 14:03:22 +0100

    Feat: Image for logo flips correctly

--- src/js/config/index.js
+++ src/js/config/index.js
@@ -274,6 +274,10 @@ class Scene {
         this.donut.children[8].material = logoMaterial;
         this.donut.children[9].material = logoMaterial;
 
+        this.donut.children[7].material.map.flipY = false;
+        this.donut.children[8].material.map.flipY = false;
+        this.donut.children[9].material.map.flipY = false;
+
       }
     });
   }
